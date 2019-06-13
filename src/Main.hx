84330fc9b872dac2221cd5036f5f125fd7775fb4
Matthijs Kamstra <matthijs@fonk.amsterdam>
2019-06-13 23:45:26 +0200

    added terminal, resources, new template

--- src/Main.hx
+++ src/Main.hx
@@ -6,6 +6,11 @@ import AST;
 using StringTools;
 
 class Main {
+	/**
+	 * 0.0.1 	initial release
+	 */
+	var VERSION:String = '0.0.1';
+
 	// resume data
 	var json:ResumeObjObj;
 	// defaults
@@ -16,13 +21,45 @@ class Main {
 	var __txt:String = '';
 	var __md:String = '';
 	var __arr:Array<String> = [];
+	// template settings
+	var settings:Dynamic; // should make this a typedef
 
-	public function new() {
+	public function new(?args:Array<String>) {
 		TARGET = Sys.getCwd().split('bin/')[1].split('/')[0]; // yep, that works in this folder structure
 		EXPORT = Path.normalize(Sys.getCwd().split('bin/')[0] + '/docs/${TARGET}'); // normal situation this would we just the `www` or `docs` folder
 		ASSETS = Path.normalize(Sys.getCwd().split('bin/')[0] + '/assets/');
 
 		trace('[${TARGET}] Working with resume.json');
+
+		Sys.println('[${TARGET}] CLI "hxLoremIpsum" ');
+
+		// create some general information/settings which can be used for template generation
+		settings = {
+			title: '[${TARGET}] minimal static site generator',
+			footer: 'Copyright &copy; ${Date.now().getFullYear()} - [mck]'
+		}
+
+		var args:Array<String> = args;
+
+		if (args.length == 0)
+			args.push('-h');
+
+		for (i in 0...args.length) {
+			var temp = args[i];
+			switch (temp) {
+				case '-i', '-init', 'init':
+					trace(haxe.Resource.getString("resumeJson"));
+				case '-v', '-version':
+					Sys.println('version: ' + VERSION);
+				case '-cd', '-folder': // isFolderSet = true;
+				case '-help', '-h':
+					showHelp();
+				case '-out', '-o':
+					writeOut();
+				default:
+					trace("case '" + temp + "': trace ('" + temp + "');");
+			}
+		}
 
 		var path = Path.normalize(ASSETS + '/resume.json');
 
@@ -43,8 +80,7 @@ class Main {
 		writeTxt(); // first txt, because markdown modifice __txt (need to fix that)
 		writeMarkdown();
 		writeHtml();
-
-		// trace(AST.ResumeObjObj);
+		writeTemplate();
 	}
 
 	function writeOut() {
@@ -75,7 +111,6 @@ class Main {
 		__txt = __txt.replace('\t', '- **'); // convert tab to list
 		__txt = __txt.replace('**- ', '\t- **'); // convert the double tab to dubble tab list
 		__txt = __txt.replace(': ', ':** '); // create bold in combintaion with previous replace
-
 
 		str += __txt;
 
@@ -122,6 +157,21 @@ class Main {
 </html>';
 
 		writeFile(EXPORT, 'resume.html', str);
+	}
+
+	function writeTemplate() {
+		var str = haxe.Resource.getString("htmlTemplate");
+		var template = new haxe.Template(str);
+
+		var settings = {
+			title: json.basics.name,
+			work: json.work
+		};
+
+		var output = template.execute(settings);
+		// return output;
+		// trace(output);
+		writeFile(EXPORT, 'resume_wip.html', output);
 	}
 
 	// ____________________________________ tools ____________________________________
@@ -186,9 +236,49 @@ class Main {
 		trace('written file: ${path}/${filename}');
 	}
 
+	function correctCLI(target:String):String {
+		var str = '';
+		switch (target) {
+			case 'neko':
+				str = 'neko main';
+			case 'cpp':
+				str = '/Main';
+			case 'cs':
+				str = 'mono Main.exe';
+			case 'java':
+				str = 'java -jar Main.jar';
+			case 'lua':
+				str = 'lua main.lua';
+			case 'node':
+				str = 'node main.js';
+			case 'python':
+				str = 'python3 main.py';
+			default:
+				str = '[xxx]';
+				trace("case '" + target + "': trace ('" + target + "');");
+		}
+		return str;
+	}
+
+	function showHelp():Void {
+		Sys.println('------------------------------------------------
+hxLoremIpsum ($VERSION)
+
+How to use (${TARGET}):
+${correctCLI(TARGET)} -out
+
+	-version / -v   : version number
+	-help / -h      : show this help
+	-folder / -cd   : path to project folder
+	-out / -o       : write readme
+------------------------------------------------
+');
+
+	}
+
 	// ____________________________________ starting point ____________________________________
 
 	static public function main():Void {
-		var main = new Main();
+		var app = new Main(Sys.args());
 	}
 }
